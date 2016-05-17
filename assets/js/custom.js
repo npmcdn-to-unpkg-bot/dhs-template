0fdc992d5d5ca88d13e9c3f09dd752368722a09d
k1p1l0 <s0ht@mail.ru>
2016-05-18 01:24:33 +0300

    Adding scrolling to anchors

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -56,6 +56,25 @@ var all = {
                 parallax: true,
                 grabCursor: true
             });
+        },
+
+        scroll: function() {
+            $("#myNavbar").on("click","a", function (event) {
+                //отменяем стандартную обработку нажатия по ссылке
+                event.preventDefault();
+
+                //забираем идентификатор бока с атрибута href
+                var id  = $(this).attr('href');
+
+                //узнаем высоту от начала страницы до блока на который ссылается якорь
+                if(id == "#home") {
+                    var top = $(id).offset().top + -80;
+                } else {
+                    var top = $(id).offset().top;
+                }
+                //анимируем переход на расстояние - top за 1500 мс
+                $('body,html').animate({scrollTop: top}, 1000);
+            });
         }
     }
 }
@@ -66,6 +85,7 @@ var init = {
     ready: function() {
         all.listitems.isotope();
         all.listitems.swiper();
+        all.listitems.scroll();
     }
 };
 
